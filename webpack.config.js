1b4c5f6b971636fd08886f752f5dc1418ce8b842
Gabriele Canepa <gabriele@canepa.io>
2018-12-15 23:19:53 +0100

    Add dotenv and minimizers

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,79 @@
+// const Dotenv = require('dotenv-webpack');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//
+// const devMode = process.env.NODE_ENV !== "production";
+//
+// module.exports = {
+//   mode: devMode ? "development" : "production",
+//   entry: ["./javascripts/index.js", "./stylesheets/index.scss"],
+//   output: {
+//     path: __dirname,
+//     filename: "./script.js"
+//   },
+//   plugins: [
+//     new Dotenv(),
+//     new MiniCssExtractPlugin({
+//       path: __dirname,
+//       filename: "./style.css"
+//     })
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.m?js$/,
+//         exclude: /(node_modules)/,
+//         loader: "babel-loader"
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: [
+//           MiniCssExtractPlugin.loader,
+//           "css-loader",
+//           "sass-loader"
+//         ]
+//       }
+//     ]
+//   }
+// };
+//
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+//
+// module.exports = {
+//   optimization: {
+//     minimizer: [
+//       new UglifyJsPlugin({
+//         cache: true,
+//         parallel: true,
+//         sourceMap: true
+//       }),
+//       new OptimizeCSSAssetsPlugin({})
+//     ]
+//   },
+//   plugins: [
+//     new MiniCssExtractPlugin({
+//       filename: "[name].css",
+//       chunkFilename: "[id].css"
+//     })
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: [
+//           MiniCssExtractPlugin.loader,
+//           "css-loader"
+//         ]
+//       }
+//     ]
+//   }
+// }
+
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 const devMode = process.env.NODE_ENV !== "production";
 
@@ -7,12 +82,24 @@ module.exports = {
   entry: ["./javascripts/index.js", "./stylesheets/index.scss"],
   output: {
     path: __dirname,
-    filename: "./script.js"
+    filename: "script.js"
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
   plugins: [
+    new Dotenv(),
     new MiniCssExtractPlugin({
       path: __dirname,
-      filename: "./style.css"
+      filename: devMode ? "style.css" : "style.[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
     })
   ],
   module: {
@@ -20,10 +107,10 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader"
+        use: "babel-loader"
       },
       {
-        test: /\.scss$/,
+        test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader",
