93447868112a8ee9eedf4b3ea1dd88a7106a00d3
Gabriele Canepa <gabriele@canepa.io>
2018-12-16 18:40:23 +0100

    Add build and deployment option, html-webpack-plugin and image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const packageJSON = require("./package.json");
 
 const devMode = process.env.NODE_ENV !== "production";
 
@@ -9,23 +12,39 @@ module.exports = {
   mode: devMode ? "development" : "production",
   entry: ["./javascripts/index.js", "./stylesheets/index.scss"],
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, "dist"),
     filename: "script.js"
   },
   optimization: {
     minimizer: [
+      new OptimizeCSSAssetsPlugin({}),
       new UglifyJsPlugin({
         cache: true,
         parallel: true,
         sourceMap: true
-      }),
-      new OptimizeCSSAssetsPlugin({})
+      })
     ]
   },
   plugins: [
     new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      favicon: "images/favicon.png",
+      hash: true,
+      showErrors: true,
+      meta: {
+        description: packageJSON.description
+      },
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    }),
     new MiniCssExtractPlugin({
-      path: __dirname,
       filename: devMode ? "style.css" : "style.[hash].css",
       chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
     })
@@ -44,6 +63,26 @@ module.exports = {
           "css-loader",
           "sass-loader"
         ]
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: "url-loader",
+        options: {
+          limit: 10 * 1024
+        }
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-url-loader",
+        options: {
+          limit: 10 * 1024,
+          noquotes: true
+        }
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: "image-webpack-loader",
+        enforce: "pre"
       }
     ]
   }
